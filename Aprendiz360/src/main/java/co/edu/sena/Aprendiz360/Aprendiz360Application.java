611a51a0accfc1f52d4ccd4ed362200139626740
demonnight245 <demonnight245@gmail.com>
2026-09-17 10:26:11 -0500

    Base SpringBoot/Angular

---
+++ Aprendiz360/src/main/java/co/edu/sena/Aprendiz360/Aprendiz360Application.java
@@ -0,0 +1,13 @@
+package co.edu.sena.Aprendiz360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aprendiz360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aprendiz360Application.class, args);
+	}
+
+}
---
+++ Aprendiz360/src/test/java/co/edu/sena/Aprendiz360/Aprendiz360ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.sena.Aprendiz360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aprendiz360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
